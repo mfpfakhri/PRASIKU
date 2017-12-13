14ed68cd946a91b1e3bb66fc54608e8c130ad493
mfpfakhri <muhammad.fakhrian@gmail.com>
2017-12-14 00:12:54 +0700

    static spinner di sign up selesai. static spinner cat breeds selesai. static spinner cat adoption selesai. static spinner upload animal selesai. static spinner adoption form received | yang mau adopt selesai. next : Fragment di 1. Activity Home ---> Cat Breeds | Upload Animal | Adoption Form Received 2. Activity Cat Adoption ---> Find Cat | Find User

--- app/src/main/java/com/example/android/prasikuv11/UserAdoptionFormActivity.java
+++ app/src/main/java/com/example/android/prasikuv11/UserAdoptionFormActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.prasikuv11;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UserAdoptionFormActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class UserAdoptionFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_adoption_form);
+
+        Spinner spinnerHouseType = (Spinner) findViewById(R.id.user_adoption_spinner_house_type);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> userAdoptionHouseTypeAdapter = ArrayAdapter.createFromResource(this, R.array.house_type_spinner, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        userAdoptionHouseTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerHouseType.setAdapter(userAdoptionHouseTypeAdapter);
     }
 }
