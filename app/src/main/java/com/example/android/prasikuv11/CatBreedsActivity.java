cdc518b2851d91a0c7ede173bf2f8f87b5c22723
mfpfakhri <muhammad.fakhrian@gmail.com>
2017-12-13 23:19:28 +0700

    static spinner di sign up selesai. static spinner cat breeds selesai. next : static spinner di 1. cat adoption 2. upload animal 3. adoption form received | yang mau adopt

--- app/src/main/java/com/example/android/prasikuv11/CatBreedsActivity.java
+++ app/src/main/java/com/example/android/prasikuv11/CatBreedsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.prasikuv11;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CatBreedsActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class CatBreedsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_breeds);
+
+        Spinner spinnerCatBreeds = (Spinner) findViewById(R.id.spinner_cat_breeds);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> catBreedsSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.cat_breeds_spinner, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        catBreedsSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerCatBreeds.setAdapter(catBreedsSpinnerAdapter);
     }
 }
